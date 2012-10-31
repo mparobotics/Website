b6ef8fcbb774f2e1a7720d2bc257c8262e6c3fa9
Brendan Boyle <brendancboyle@me.com>
2012-10-31 13:54:14 -0500

    Mobile toggle

--- header.php
+++ header.php
@@ -11,7 +11,9 @@ Michael Wilke
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <?php 
 if ($uacheck->ismobile()) {
-	echo '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">';
+	if (false) {
+		echo '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">';
+	}
 }
 if (isset($desc)) {
 	echo '<meta name="description" content="'.$desc.'">';
