947e96b27cd3977d3b344a5903a13a2232dd9ad3
Connor O <colson13@moundsparkacademy.org>
2012-11-03 11:32:32 -0500

    To validate the mobile footer
    Really Guys?  http://validator.w3.org
    
    Use it.

--- mobilefooter.php
+++ mobilefooter.php
@@ -1,7 +1,7 @@
 <div id="mobilefooter">
-<a href="https://m.facebook.com/team3926"><img class="footeri1" src="mobile_footer_images/images/facebook.png" /></a>
-<a href="http://m.youtube.com/team3926"><img class="footeri2" src="mobile_footer_images/images/youtube.png" /> </a>
-<a href="https://twitter.com/#!/MPARobotics"><img class="footeri3" src="mobile_footer_images/images/twitter.png" style="width: 50px; height: 50px;"/> </a>
+<a href="https://m.facebook.com/team3926"><img class="footeri1" src="mobile_footer_images/images/facebook.png" alt="Find us on Facebook!" /></a>
+<a href="http://m.youtube.com/team3926"><img class="footeri2" src="mobile_footer_images/images/youtube.png" alt="Find us on Youtube!" /> </a>
+<a href="https://twitter.com/#!/MPARobotics"><img class="footeri3" src="mobile_footer_images/images/twitter.png" style="width: 50px; height: 50px;" alt="Find us on Twitter!" /> </a>
 
 <div id="mobileNav">
 <ul>
