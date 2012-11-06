88e0fa0b5bc05b17c9ed173dbd60f85979cf94ec
Brendan Boyle <brendancboyle@me.com>
2012-11-06 13:45:26 -0600

    New index layout

--- index.php
+++ index.php
@@ -8,11 +8,51 @@ flush();
 
 <div id="content" class="navDisabled">
 
+<div class="slidenav">
+<div class="slidenavinfo">
+<h1>Title</h1>
+<h2>Subtitle</h2>
+<p>Content contentcontent content content content content content content content content content content content content</p>
+</div>
+</div>
 
 <div class="split">
 <p class="splittext">This is a split</p>
 </div>
 
+<div class="metronav" style="height: 325px;">
+
+<div class="metroitem" style="background:orange;">
+<img src="images/arrow.svg" alt="Arrow" class="metroarrow" />
+<p class="metrodescription">Sponsors</p>
+</div>
+
+<div class="metroitem" style="background:red;">
+<img src="images/arrow.svg" alt="Arrow" class="metroarrow" />
+<p class="metrodescription">Sponsors</p>
+</div>
+
+<div class="metroitem" style="background:purple;">
+<img src="images/arrow.svg" alt="Arrow" class="metroarrow" />
+<p class="metrodescription">Sponsors</p>
+</div>
+
+<div class="metroitem" style="background:green;">
+<img src="images/arrow.svg" alt="Arrow" class="metroarrow" />
+<p class="metrodescription">Sponsors</p>
+</div>
+
+<div class="metroitem" style="background:blue;">
+<img src="images/arrow.svg" alt="Arrow" class="metroarrow" />
+<p class="metrodescription">Sponsors</p>
+</div>
+
+<div class="metroitem" style="background:pink;">
+<img src="images/arrow.svg" alt="Arrow" class="metroarrow" />
+<p class="metrodescription">Sponsors</p>
+</div>
+
+</div>
 
 <!--
 <div id="mobilecontent">
@@ -20,8 +60,6 @@ flush();
 <br />
 <?php include('mobilefooter.php'); ?>
 </div> -->
-
-<!-- End Content Exp -->
 
 </div>
 <?php 
