f9bd11fc7a5023554a3163fc029725dd2f3c2c3a
Brendan Boyle <brendancboyle@me.com>
2012-11-08 15:02:58 -0600

    Updated arrows to call the new js

--- Team.php
+++ Team.php
@@ -8,22 +8,22 @@ include('header.php');
 
 <div class="interactive" id="team1">
     <img src="images/logo.svg" class="logo" alt="Logo" style="width: 500px;" />
-    <a href="#team2" onclick="alert('Next Slide!');"><img src="images/arrowwhite.svg" id="team1arrow" alt="Next Slide" style="float: right; width: 200px;" /></a>
+    <a href="#team2" onclick="interactiveArrow('team',2,1);"><img src="images/arrowwhite.svg" id="team1arrow" alt="Next Slide" style="float: right; width: 200px;" /></a>
 </div>
     
 <div class="interactive" id="team2">
     <img src="images/logo.svg" alt="Logo" />
-    <a href="#team3" onclick="alert('Next Slide!');"><img src="images/arrowwhite.svg" id="team2arrow" alt="Next Slide" /></a>
+    <a href="#team3" onclick="interactiveArrow('team',3,2);"><img src="images/arrowwhite.svg" id="team2arrow" alt="Next Slide" /></a>
 </div>
     
 <div class="interactive" id="team3">
     <img src="images/logo.svg" alt="Logo" />
-    <a href="#team4" onclick="alert('Next Slide!');"><img src="images/arrowwhite.svg" id="team3arrow" alt="Next Slide" /></a>
+    <a href="#team4" onclick="interactiveArrow('team',4,3);"><img src="images/arrowwhite.svg" id="team3arrow" alt="Next Slide" /></a>
 </div>
     
 <div class="interactive" id="team4">
     <img src="images/logo.svg" alt="Logo" />
-    <a href="#team1" onclick="alert('Back to the Beginning!');"><img src="images/arrowwhite.svg" id="team4arrow" alt="Next Slide" /></a>
+    <a href="#team1" onclick="interactiveArrow('team',1,4);"><img src="images/arrowwhite.svg" id="team4arrow" alt="Next Slide" /></a>
 </div>
 
 </div>
