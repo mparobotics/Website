e83d80fb474a6835c548203f606f49e4e45f45d7
Brendan Boyle <brendancboyle@me.com>
2012-11-20 14:00:40 -0600

    Updated sponsors page

--- Sponsors.php
+++ Sponsors.php
@@ -8,31 +8,28 @@ flush();
 <div id="content">
     <div class="metronav" style="height: 325px; margin-top: 60px;">
 
-        <a href="/Media" class="metroitem" style="background: url('images/Sponsors/Metro/jcpenny.svg');">
-            <p class="metrodescription">JCPenny</p>
+        <a href="http://www.jcpenney.com/dotcom/index.jsp" class="metroitem" style="background: url('images/Sponsors/Metro/jcpenny.svg');">
+            <p class="metrodescription">JCPenney</p>
         </a>
         
-        <a href="/Competitions" class="metroitem" style="background: url('images/Sponsors/Metro/nasa.svg');">
+        <a href="http://www.nasa.gov" class="metroitem" style="background: url('images/Sponsors/Metro/nasa.svg');">
             <p class="metrodescription">NASA</p>
         </a>
         
-        <a href="/FIRST" class="metroitem" style="background: url('images/Sponsors/Metro/reell.svg');">
+        <a href="http://www.reell.com" class="metroitem" style="background: url('images/Sponsors/Metro/reell.svg');">
             <p class="metrodescription">Reell</p>
         </a>
         
         <a href="Sponsors" class="metroitem" style="background:green;">
             <p class="metrodescription">Atreium</p>
-            <img src="images/MPAror/arrow.svg" alt="Arrow" class="metroarrow" />
         </a>
         
         <a href="store" class="metroitem" style="background:blue;">
             <p class="metrodescription">SMC</p>
-            <img src="images/MPAror/arrow.svg" alt="Arrow" class="metroarrow" />
         </a>
         
         <a href="/News" class="metroitem" style="background:pink;">
             <p class="metrodescription">Underground Printing</p>
-            <img src="images/MPAror/arrow.svg" alt="Arrow" class="metroarrow" />
         </a>
     
     </div>
