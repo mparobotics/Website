afc06e447dc36c240b93ba0b22b422b2da4c09b5
Brendan Boyle <brendancboyle@me.com>
2012-12-29 19:25:19 -0600

    Added nodes to robot page

--- Robot.php
+++ Robot.php
@@ -8,21 +8,41 @@ include('header.php');
 
     <div class="selectbar">
         <p>Select a year:</p>
-        <p><a href="#">2012</a>|<a href="#">2013</a></p>
+        <p><a href="#" onClick="document.getElementById('2013').style.display = 'none'; document.getElementById('2012').style.display = 'block';">2012</a>|<a href="#" onClick="document.getElementById('2012').style.display = 'none'; document.getElementById('2013').style.display = 'block';">2013</a></p>
     </div>
     
-    <div id="robotImage">
-        <a href="#" class="node"></a>
-    </div>
-    
-    <div id="robotContent">
-        <div class="contentStatic">
-            <h1>Title</h1>
-            <h2>Subtitle</h2>
-            <p>Content</p>
+    <div id="2012">
+        <div id="robotImage" style="background: url();">
+            <div class="node" onclick="document.getElementById('2012content').innerHTML = 'Slide 1';"></div>
+            <div class="node" onclick="document.getElementById('2012content').innerHTML = 'Slide 2';"></div>
         </div>
         
-        <div class="contentStub">
+        <div id="robotContent">
+            <div class="contentStatic">
+                <h1>Rebound Rumble</h1>
+                <h2>2012 Robot Name Here</h2>
+                <p id="2012content">Content</p>
+            </div>
+            
+            <div class="contentStub">
+            </div>
+        </div>
+    </div>
+    
+    <div id="2013" style="display: none;">
+        <div id="robotImage" style="background: url();">
+            <!--<div class="node"></div>-->
+        </div>
+        
+        <div id="robotContent">
+            <div class="contentStatic">
+                <h1>Classified</h1>
+                <h2>Classified</h2>
+                <p id="2013content">Classified</p>
+            </div>
+            
+            <div class="contentStub">
+            </div>
         </div>
     </div>
     
