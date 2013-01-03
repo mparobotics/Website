75c2b5e69087e8471b3098ce8f286e8fc01c7833
Brendan Boyle <brendancboyle@me.com>
2013-01-03 00:32:50 -0600

    Added videos to the media page

--- Media.php
+++ Media.php
@@ -4,10 +4,12 @@ $pageTitle = "MPA Robotics - Media";
 include('header.php');
 ?>
 
-<div id="content" style="padding: 0px; padding-top: 40px;">
-
-<iframe src="/zenphoto" class="fillFrame">Sorry, your browser does not support iFrame</iframe>
-
+<div id="content" style="padding: 0px;">
+    <h1 style="font-size: 400%; margin: 5px">Videos:</h1>
+    <iframe width="560" height="315" src="http://www.youtube.com/embed/i1QyM9WTF18?rel=0" frameborder="0" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="http://www.youtube.com/embed/vYuOKb3gO7E?rel=0" frameborder="0" allowfullscreen></iframe>
+    <h1 style="font-size: 400%; margin: 5px">Team Photos:</h1>
+    <iframe src="/zenphoto" class="fillFrame">Sorry, your browser does not support iFrame</iframe>
 </div>
 <?php 
 $footerstuff = NULL;
