79669d83b5e3a911735c1dc0db84366daa6e2a51
Brendan Boyle <brendancboyle@me.com>
2013-01-11 17:48:58 -0600

    Fixed links

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php flush(); ?>
 <div id="sociallinks">
 <a href="https://twitter.com/mparobotics"><img src="/images/Elements/twitter.svg" alt="Find us on Twitter!" class="socialitem" style=" margin-top: 13px; " /></a><br />
-<a href="https://facebook.com/team3926.org"><img src="/images/Elements/facebook.svg" alt="Find us on Facebook!" class="socialitem" /></a><br />
+<a href="https://facebook.com/team3926"><img src="/images/Elements/facebook.svg" alt="Find us on Facebook!" class="socialitem" /></a><br />
 <a href="https://plus.google.com/108009739203475798666/posts"><img src="/images/Elements/gplus.svg" alt="Find us on Google Plus!" class="socialitem" /></a><br />
 <a href="http://www.youtube.com/user/team3926"><img src="/images/Elements/youtube.svg" alt="Find us on YouTube!" class="socialitem" /></a><br />
 </div>
