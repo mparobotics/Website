e6bf90cc09f82f9e13d297aeefaa9953ba0037ca
Brendan Boyle <brendancboyle@me.com>
2013-01-18 17:45:53 -0600

    Competitions page

---
+++ Competitions.php
@@ -0,0 +1,17 @@
+<?php 
+$headerstuff = NULL;
+$pageTitle = "MPA Robotics - The Team";
+include('header.php');
+?>
+
+<div id="content" class="navDisabled">
+
+<div class="interactive" id="team1">
+    <h1>Coming Soon!</h1>
+</div>
+
+</div>
+<?php 
+$footerstuff = NULL;
+include('footer.php');
+?>
