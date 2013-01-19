3235957b0cac7e00bc089f9eb29afb73d95d2254
Michael Wilke <mwilke16@moundsparkacademy.org>
2013-01-18 19:01:08 -0600

    Revert "Competitions page"
    This reverts commit 7b94b72a214ea13a2d19065ac2d52a55f0a05df0.

--- Competitions.php
+++ Competitions.php
@@ -7,14 +7,7 @@ include('header.php');
 <div id="content" class="navDisabled">
 
 <div class="interactive" id="team1">
-	<div style="float: left; width: 50%;">
-		<iframe width="455" height="284" align="left" src="https://www.youtube.com/embed/itHNW2OFr4Y?rel=0;showinfo=0;HD=1;" frameborder="0" allowfullscreen></iframe>
-		<div style="width: 100%; height: 100px; float: left; padding: 3px; padding-right: 2px; text-align: left;"><p style="float: left; width: 80%">Omg the text goes here. The text is magic, the text is important. Brendan is the website god because he is brilliant and magically delicious. Kind of like lucky charms....</p><img src="images/MPAror/arrowwhite.svg" alt="Arrow" style="float: right" /></div>
-	</div>
-	<div stlye="float: right; width: 50%">
-		<h1><u>Ultimate Ascent</u></h1>
-    	<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Mariucci+Arena,+Southeast+4th+Street,+Minneapolis,+MN&amp;aq=0&amp;oq=mariuc&amp;sll=46.468133,-93.361816&amp;sspn=8.489718,17.226562&amp;ie=UTF8&amp;hq=Mariucci+Arena,+Southeast+4th+Street,+Minneapolis,+MN&amp;ll=44.978437,-93.228403&amp;spn=0.015786,0.032015&amp;t=h&amp;deg=270&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Mariucci+Arena,+Southeast+4th+Street,+Minneapolis,+MN&amp;aq=0&amp;oq=mariuc&amp;sll=46.468133,-93.361816&amp;sspn=8.489718,17.226562&amp;ie=UTF8&amp;hq=Mariucci+Arena,+Southeast+4th+Street,+Minneapolis,+MN&amp;ll=44.978437,-93.228403&amp;spn=0.015786,0.032015&amp;t=h&amp;deg=270" style="color:#0000FF;text-align:left">View Larger Map</a></small>
-    </div>
+    <h1>Coming Soon!</h1>
 </div>
 
 </div>
