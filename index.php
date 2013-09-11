305b0ea3318ec6a8701fb8635226738604bde081
Michael Wilke <mwilke16@moundsparkacademy.org>
2013-09-11 13:21:36 -0500

    up2date with server

--- index.php
+++ index.php
@@ -11,13 +11,12 @@ flush();
 <div class="slidenav" style="background: url(images/SlideNav/UltimateAscent.svg)"> 
 <div class="slidenavinfo">
 </br>
-<h1>Summer 2013</h1>
+<h1>FRC 2014</h1>
 </br>
-<h2>Woodbury Days</h2>
+<h2>Kick Off</h2>
 </br>
 </br>
-<p>Come join us August 23-25th at Woodbury Days in Ojibway Park for tons of Robotic fun!</p>
-<p align="right"><a href="https://www.google.com/maps?q=Ojibway+park&sll=44.9638216,-93.04463450000002&sspn=0.16228375218624327,0.43821082005258527&t=h&dg=opt&hq=Ojibway+park&z=18">Map to Woodbury days</a>
+<p>Kick off is rapidly approaching! January 4th the 2014 game will be released!</p>
 </br>
 </div>
 </div>
@@ -26,7 +25,7 @@ flush();
     <!--<p class="splittext">This is a split</p>-->
 </div>
 
-<div class="metronav" style="height: 325px;">
+<div class="metronav" style="height: 385px;">
 
     <a href="Media" class="metroitem" style="background-image:url('/images/Metro/Media.png');">
         <p class="metrodescription">Media</p>
@@ -58,10 +57,14 @@ flush();
         <img src="images/MPAror/arrow.svg" alt="Arrow" class="metroarrow" />
     </a>
 
-	<a href="Careers" class="metroitem" style="background-image:url('#');">
-		<p class="metrodesciption">Careers in Robotics</p>
-		<img src="#" alt="Arrow" class="metroarrow" />
-	</a>
+	
+	<a href="careers.php" class="metroitemlong" style="background-image:url('/images/Metro/careers.svg');">
+       <img src="images/MPAror/arrowwhite.svg" width="70%" height="70%" alt="Arrow" class="metroarrowsmall" />
+    </a>
+    
+    <a href="newsletter.php" class="metroitemlong" style="background-image:url('/images/Metro/newsletter.svg');">
+        <img src="images/MPAror/arrowwhite.svg" width="70%" height="70%" alt="Arrow" class="metroarrowsmall" />
+    </a>
 	
 </div>
 
