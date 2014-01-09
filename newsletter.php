25263431c3335f1a4b5eca9881ecca62e7b5126b
Michael Wilke <mwilke16@moundsparkacademy.org>
2014-01-09 13:04:58 -0600

    All Links fixed. Images moved to better location. Aerial Assist Logo added

--- newsletter.php
+++ newsletter.php
@@ -9,27 +9,27 @@ include('header.php');
 	
     <div class="metronav" style="height: 700px;">
 
-        <a href="newsletters/Newsletter-8-13-2013.pdf" class="newsitem" style="background: url('images/newsletter/previews/Aug13.png'); background-size:242px 300px; background-position: 0px 21px;">
+        <a href="Files/Newsletter-8-13-2013.pdf" class="newsitem" style="background: url('images/newsletters/Aug13.png'); background-size:242px 300px; background-position: 0px 21px;">
             <p class="metrodescription">August 13, 2013</p>
         </a>
         
-        <a href="newsletters/Newsletter-5-7-2013.pdf" class="newsitem" style="background: url('images/newsletter/previews/May7.png'); background-size:242px 300px; background-position: 0px 21px;">
+        <a href="Files/Newsletter-5-7-2013.pdf" class="newsitem" style="background: url('images/newsletters/May7.png'); background-size:242px 300px; background-position: 0px 21px;">
             <p class="metrodescription">May 7, 2013</p>
         </a>
         
-        <a href="newsletters/Newsletter-2-12-2013.pdf" class="newsitem" style="background: url('images/newsletter/previews/Feb12.png'); background-size:242px 300px; background-position: 0px 21px;">
+        <a href="Files/Newsletter-2-12-2013.pdf" class="newsitem" style="background: url('images/newsletters/Feb12.png'); background-size:242px 300px; background-position: 0px 21px;">
             <p class="metrodescription">February 12, 2013</p>
         </a>
         
-        <a href="newsletters/Newsletter-1-29-2013.pdf" class="newsitem" style="background: url('images/newsletter/previews/Jan29.png'); background-size:242px 300px; background-position: 0px 21px;">
+        <a href="Files/Newsletter-1-29-2013.pdf" class="newsitem" style="background: url('images/newsletters/Jan29.png'); background-size:242px 300px; background-position: 0px 21px;">
             <p class="metrodescription">January 29, 2013</p>
         </a>
         
-        <a href="newsletters/Newsletter1-15-2013.pdf" class="newsitem" style="background: url('images/newsletter/previews/Jan15.png'); background-size:242px 300px; background-position: 0px 21px;">
+        <a href="Files/Newsletter1-15-2013.pdf" class="newsitem" style="background: url('images/newsletters/Jan15.png'); background-size:242px 300px; background-position: 0px 21px;">
             <p class="metrodescription">January 15, 2013</p>
         </a>
         
-        <a href="newsletters/Newsletter-2-1-2012.pdf" class="newsitem" style="background: url('images/newsletter/previews/Feb1.png'); background-size:242px 300px; background-position: 0px 21px;">
+        <a href="Files/Newsletter-2-1-2012.pdf" class="newsitem" style="background: url('images/newsletters/Feb1.png'); background-size:242px 300px; background-position: 0px 21px;">
             <p class="metrodescription">February 1st, 2012</p>
         </a>
     
